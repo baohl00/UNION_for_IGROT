69ac5a89eeae6b1576489cc42131bac7e08b1dc9
Hoang-Bao Le <72609761+baohl00@users.noreply.github.com>
2025-10-23 10:50:55 +0100

    Clean up model name assignments in cir_inference.sh
    Removed commented-out model name assignments for clarity.

--- scripts/cir_inference.sh
+++ scripts/cir_inference.sh
@@ -12,13 +12,7 @@ llava=with
 note=${llava}_llava_${target_type}
 comment=_5k_${note}_${encoder}_fuse_extended
 name=${model}_${epochs}_epo$comment
-#name=final_model_clip_large_2_epo_5k_with_llava_union_both.pth
-#name=final_model_clip_base_2_epo_5k_without_llava_original_both.pth
-#name=final_model_${model}_2_epo_5k_${llava}_llava_${target_type}_both
-#name=final_model_blip_2_epo_5k_without_llava_union_both.pth
-#name=final_model_clip_base_2_epo_5k_without_llava_union_both.pth
-#name=final_model_clip_base_2_epo_5k_with_llava_union_both_visionprojector.pth
-#name=final_model_clip_base_2_epo_5k_with_llava_union_both_sum.pth
+
 save_path=./ckpt/final_model_$name
 eval_load_path=./ckpt/final_model_$name.pth
 python3 -m torch.distributed.run --nnodes=1 --nproc_per_node=1 --master_port=25033 \
